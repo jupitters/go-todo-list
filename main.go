7f8db15d5f01bf1accc212bc8998cc42991e6623
jupitters <sampaiobale@gmail.com>
2025-09-16 01:54:26 -0300

    giving json

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ func main() {
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello Fiber!")
+		return c.Status(200).JSON(fiber.Map{"msg": "Ola Fiber!"})
 	})
 
 	log.Fatal(app.Listen(":4000"))
