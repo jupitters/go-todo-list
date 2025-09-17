eb9f8bfdbac8fa507fd3ea31e9633e1f50d0d16a
jupitters <sampaiobale@gmail.com>
2025-09-17 01:18:19 -0300

    navbar

--- client/todo-list/src/components/Navbar.tsx
+++ client/todo-list/src/components/Navbar.tsx
@@ -1,6 +1,7 @@
-import { Box, Flex, Button, useColorModeValue, useColorMode, Text, Container } from "@chakra-ui/react";
+import { Box, Flex, Button, Text, Container } from "@chakra-ui/react";
 import { IoMoon } from "react-icons/io5";
 import { LuSun } from "react-icons/lu";
+import { useColorMode, useColorModeValue } from "./ui/color-mode";
 
 export default function Navbar() {
 	const { colorMode, toggleColorMode } = useColorMode();
@@ -9,19 +10,6 @@ export default function Navbar() {
 		<Container maxW={"900px"}>
 			<Box bg={useColorModeValue("gray.400", "gray.700")} px={4} my={4} borderRadius={"5"}>
 				<Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
-					{/* LEFT SIDE */}
-					<Flex
-						justifyContent={"center"}
-						alignItems={"center"}
-						gap={3}
-						display={{ base: "none", sm: "flex" }}
-					>
-						<img src='/react.png' alt='logo' width={50} height={50} />
-						<Text fontSize={"40"}>+</Text>
-						<img src='/go.png' alt='logo' width={40} height={40} />
-						<Text fontSize={"40"}>=</Text>
-						<img src='/explode.png' alt='logo' width={50} height={50} />
-					</Flex>
 
 					{/* RIGHT SIDE */}
 					<Flex alignItems={"center"} gap={3}>
