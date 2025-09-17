2b804252f3bc28f4079014e5af1efb323db00bd4
jupitters <sampaiobale@gmail.com>
2025-09-16 22:35:16 -0300

    godotenv

--- main.go
+++ main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
 )
 
 type Todo struct {
@@ -15,6 +17,12 @@ type Todo struct {
 
 func main() {
 	app := fiber.New()
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	PORT := os.Getenv("PORT")
 
 	todos := []Todo{}
 
@@ -70,5 +78,5 @@ func main() {
 		return c.Status(404).JSON(fiber.Map{"error": "Todo not found!"})
 	})
 
-	log.Fatal(app.Listen(":4000"))
+	log.Fatal(app.Listen(PORT))
 }
