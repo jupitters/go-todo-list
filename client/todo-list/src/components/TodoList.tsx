7cf63c3853c2b8afeef6a3fa7fae101586ef303d
jupitters <sampaiobale@gmail.com>
2025-09-17 14:20:43 -0300

    querying fetching data

--- client/todo-list/src/components/TodoList.tsx
+++ client/todo-list/src/components/TodoList.tsx
@@ -1,6 +1,6 @@
 import { Flex, Spinner, Stack, Text } from "@chakra-ui/react";
-import { useState } from "react";
 import TodoItem from "./TodoItem";
+import { useQuery } from "@tanstack/react-query";
 
 export type Todo = {
     _id: number;
@@ -9,29 +9,23 @@ export type Todo = {
 };
 
 const TodoList = () => {
-	const [isLoading, setIsLoading] = useState(true);
-	const todos = [
-		{
-			_id: 1,
-			body: "Buy groceries",
-			completed: true,
-		},
-		{
-			_id: 2,
-			body: "Walk the dog",
-			completed: false,
-		},
-		{
-			_id: 3,
-			body: "Do laundry",
-			completed: false,
-		},
-		{
-			_id: 4,
-			body: "Cook dinner",
-			completed: true,
-		},
-	];
+    const {data:todos, isLoading} = useQuery<Todo[]>({
+        queryKey: ["todos"],
+
+        queryFn: async () => {
+            try{
+                const res = await fetch("http://localhost:5000/api/todos")
+                const data = await res.json()
+
+                if(!res.ok){
+                    throw new Error(data.error || "Something went wrong")
+                }
+                return data || []
+            } catch (err) {
+                console.log(err)
+            }
+        }
+    })
 	return (
 		<>
 			<Text fontSize={"4xl"} textTransform={"uppercase"} fontWeight={"bold"} textAlign={"center"} my={2}>
